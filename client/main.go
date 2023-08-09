dadeb0275d1d8489abb4babf8cd348fe68e0ce1b
Darkoment <Darkoment@yandex.ru>
2023-08-09 14:26:01 +0300

    First commit, test version

---
+++ client/main.go
@@ -0,0 +1,258 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+
+	pb "github.com/darkoment/go-grpc-crud-api/proto"
+	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+)
+
+type Book struct {
+	BookID  string `json:"book_id"`
+	Name    string `json:"name"`
+	Year    string `json:"year"`
+	Edition string `json:"edition"`
+}
+
+type Author struct {
+	AuthorID  string `json:"author_id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+func main() {
+	flag.Parse()
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+	client := pb.NewBookServiceClient(conn)
+
+	r := gin.Default()
+
+	r.GET("/book", func(ctx *gin.Context) {
+		res, err := client.GetBooks(ctx, &pb.ReadBooksRequest{})
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err,
+			})
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"Book": res.Books,
+		})
+	})
+
+	r.GET("/book/:book_id", func(ctx *gin.Context) {
+		book_id := ctx.Param("book_id")
+		res, err := client.GetBook(ctx, &pb.ReadBookRequest{Bookid: book_id})
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"message ": err.Error(),
+			})
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"book": res.Book,
+		})
+	})
+
+	r.POST("/book", func(ctx *gin.Context) {
+		var book Book
+		err := ctx.ShouldBind(&book)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err,
+			})
+			return
+		}
+		data := &pb.Book{
+			Bookid:  book.BookID,
+			Name:    book.Name,
+			Year:    book.Year,
+			Edition: book.Edition,
+		}
+		res, err := client.CreateBook(ctx, &pb.CreateBookRequest{
+			Book: data,
+		})
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err,
+			})
+			return
+		}
+		ctx.JSON(http.StatusCreated, gin.H{
+			"book": res.Book,
+		})
+	})
+
+	r.PUT("/book/:book_id", func(ctx *gin.Context) {
+		var book Book
+		err := ctx.ShouldBind(&book)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		res, err := client.UpdateBook(ctx, &pb.UpdateBookRequest{
+			Book: &pb.Book{
+				Bookid:  book.BookID,
+				Name:    book.Name,
+				Year:    book.Year,
+				Edition: book.Edition,
+			},
+		})
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err,
+			})
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"book": res.Book,
+		})
+		return
+	})
+
+	r.DELETE("book/:book_id", func(ctx *gin.Context) {
+		book_id := ctx.Param("book_id")
+		res, err := client.DeleteBook(ctx, &pb.DeleteBookRequest{Bookid: book_id})
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		if res.Success == true {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "Book deleted successfully",
+			})
+			return
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": "error deleting book",
+			})
+			return
+		}
+	})
+
+	r.GET("/author", func(ctx *gin.Context) {
+		res, err := client.GetAuthors(ctx, &pb.ReadAuthorsRequest{})
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err,
+			})
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"Authors": res.Authors,
+		})
+	})
+
+	r.GET("/author/:author_id", func(ctx *gin.Context) {
+		author_id := ctx.Param("author_id")
+		res, err := client.GetAuthor(ctx, &pb.ReadAuthorRequest{Authorid: author_id})
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"message ": err.Error(),
+			})
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"author": res.Author,
+		})
+	})
+
+	r.POST("/author", func(ctx *gin.Context) {
+		var author Author
+		err := ctx.ShouldBind(&author)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err,
+			})
+			return
+		}
+		data := &pb.Author{
+			Authorid:  author.AuthorID,
+			FirstName: author.FirstName,
+			LastName:  author.LastName,
+		}
+		res, err := client.CreateAuthor(ctx, &pb.CreateAuthorRequest{
+			Author: data,
+		})
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err,
+			})
+			return
+		}
+		ctx.JSON(http.StatusCreated, gin.H{
+			"author": res.Author,
+		})
+	})
+
+	r.PUT("/author/:author_id", func(ctx *gin.Context) {
+		var author Author
+		err := ctx.ShouldBind(&author)
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		res, err := client.UpdateAuthor(ctx, &pb.UpdateAuthorRequest{
+			Author: &pb.Author{
+				Authorid:  author.AuthorID,
+				FirstName: author.FirstName,
+				LastName:  author.LastName,
+			},
+		})
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err,
+			})
+			return
+		}
+		ctx.JSON(http.StatusOK, gin.H{
+			"author": res.Author,
+		})
+		return
+	})
+
+	r.DELETE("author/:author_id", func(ctx *gin.Context) {
+		author_id := ctx.Param("author_id")
+		res, err := client.DeleteAuthor(ctx, &pb.DeleteAuthorRequest{Authorid: author_id})
+		if err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		if res.Success == true {
+			ctx.JSON(http.StatusOK, gin.H{
+				"message": "Author deleted successfully",
+			})
+			return
+		} else {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": "error deleting author",
+			})
+			return
+		}
+	})
+
+	r.Run(":5000")
+
+}
---
+++ server/main.go
@@ -0,0 +1,320 @@
+/*
+Сторона сервера:
+Этот модуль содержит код для реализации на стороне сервера,
+а также подключение к базе данных и операций с ней
+*/
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/darkoment/go-grpc-crud-api/proto"
+	"github.com/google/uuid"
+
+	"google.golang.org/grpc"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// Функция инициализирующая клиент базы данных
+func init() {
+	DatabaseConnection()
+}
+
+var DB *gorm.DB
+var err error
+
+type Test struct {
+	Name string `gorm:"primarykey"`
+}
+
+// Структура для созадния таблицы Book
+// С ее атрибутами
+type Book struct {
+	BookID  string `gorm:"primarykey"`
+	Name    string
+	Year    string
+	Edition string
+	Authors []*Author `gorm:"many2many:book_author"`
+}
+
+// Структура для создания таблицы author
+// С ее атрибутами
+type Author struct {
+	AuthorID  string `gorm:"primarykey"`
+	FirstName string
+	LastName  string
+	Books     []*Book `gorm:"many2many:book_author"`
+}
+
+// Структура для создания смежной таблицы bookauthor
+// С ее атрибутами
+type BooksAuthors struct {
+	BookID   string `gorm:"primarykey"`
+	AuthorID string `gorm:"primarykey"`
+}
+
+// Интерфейс для названия таблиц
+// т.к. GORM добавляет окончание s к названиям таблиц
+type Tabler interface {
+	TableName() string
+}
+
+func (Book) TableName() string {
+	return "book"
+}
+
+func (Author) TableName() string {
+	return "author"
+}
+
+//Функция для установления соединения
+
+func DatabaseConnection() {
+	host := "localhost"
+	port := "3306"
+	dbName := "test"
+	dbUser := "TestAdmin"
+	password := "TestOnGo"
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbUser,
+		password,
+		host,
+		port,
+		dbName,
+	)
+	// Передаем dsn в gorm
+	// подробнее https://gorm.io/ru_RU/docs/connecting_to_the_database.html
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	DB.AutoMigrate(Book{})
+
+	//DB.Migrator().RenameTable("books", "book")
+	//DB.Migrator().RenameTable("authors", "author")
+	//DB.Migrator().CurrentDatabase()
+	if err != nil {
+		log.Fatal("Error connection to the database ", err)
+	}
+	fmt.Println("Database connection successful...")
+}
+
+// Создание gRPC сервера
+var (
+	port = flag.Int("port", 50051, "gRPC server port")
+)
+
+type server struct {
+	pb.UnimplementedBookServiceServer
+}
+
+// Метод отвечающий за создание записи о книге в таблицу Book
+func (*server) CreateBook(ctx context.Context, req *pb.CreateBookRequest) (*pb.CreateBookResponse, error) {
+	fmt.Println("Create Book")
+	book := req.GetBook()
+	book.Bookid = uuid.New().String()
+
+	data := Book{
+		BookID:  book.GetBookid(),
+		Name:    book.GetName(),
+		Year:    book.GetYear(),
+		Edition: book.GetEdition(),
+	}
+
+	res := DB.Create(&data)
+	if res.RowsAffected == 0 {
+		return nil, errors.New("Book creation unsuccessful")
+	}
+	return &pb.CreateBookResponse{
+		Book: &pb.Book{
+			Bookid:  book.GetBookid(),
+			Name:    book.GetName(),
+			Year:    book.GetYear(),
+			Edition: book.GetEdition(),
+		},
+	}, nil
+}
+
+// Метод отвечающий за получение записи о книге из таблицы Book
+func (*server) GetBook(ctx context.Context, req *pb.ReadBookRequest) (*pb.ReadBookResponse, error) {
+	fmt.Println("Read Book", req.GetBookid())
+	var book Book
+	res := DB.Find(&book, "book_id = ?", req.GetBookid())
+	if res.RowsAffected == 0 {
+		return nil, errors.New("Book not found")
+	}
+	return &pb.ReadBookResponse{
+		Book: &pb.Book{
+			Bookid:  book.BookID,
+			Name:    book.Name,
+			Year:    book.Year,
+			Edition: book.Edition,
+		},
+	}, nil
+}
+
+// Метод отвечающий за получения записей книг из таблицы Book
+func (*server) GetBooks(ctx context.Context, req *pb.ReadBooksRequest) (*pb.ReadBooksResponse, error) {
+	fmt.Println("Read Books")
+	book := []*pb.Book{}
+	res := DB.Find(&book)
+	if res.RowsAffected == 0 {
+		return nil, errors.New("Books not found")
+	}
+	return &pb.ReadBooksResponse{
+		Books: book,
+	}, nil
+}
+
+// Метод отвечающий за обновление записи о книге в таблице Book
+func (*server) UpdateBook(ctx context.Context, req *pb.UpdateBookRequest) (*pb.UpdateBookResponse, error) {
+	fmt.Println("Update Book")
+	var book Book
+	reqBook := req.GetBook()
+
+	res := DB.Model(&book).Where("book_id=?", reqBook.Bookid).Updates(
+		Book{Name: reqBook.Name, Year: reqBook.Year, Edition: reqBook.Edition})
+
+	if res.RowsAffected == 0 {
+		return nil, errors.New("Books not found")
+	}
+
+	return &pb.UpdateBookResponse{
+		Book: &pb.Book{
+			Bookid:  book.BookID,
+			Name:    book.Name,
+			Year:    book.Year,
+			Edition: book.Edition,
+		},
+	}, nil
+}
+
+// Метод отвечающий за удаление записи о книге из таблицы Book (удаление происходит по индексу)
+func (*server) DeleteBook(ctx context.Context, req *pb.DeleteBookRequest) (*pb.DeleteBookResponse, error) {
+	fmt.Println("Delete Book")
+	var book Book
+	res := DB.Where("book_id=?", req.GetBookid()).Delete(&book)
+	if res.RowsAffected == 0 {
+		return nil, errors.New("Book not found")
+	}
+
+	return &pb.DeleteBookResponse{
+		Success: true,
+	}, nil
+}
+
+// Метод отвечающий за создание автора в таблице Author
+func (*server) CreateAuthor(ctx context.Context, req *pb.CreateAuthorRequest) (*pb.CreateAuthorResponse, error) {
+	fmt.Println("Create Author")
+	author := req.GetAuthor()
+	author.Authorid = uuid.New().String()
+
+	data := Author{
+		AuthorID:  author.GetAuthorid(),
+		FirstName: author.GetFirstName(),
+		LastName:  author.GetLastName(),
+	}
+
+	res := DB.Create(&data)
+	if res.RowsAffected == 0 {
+		return nil, errors.New("Author creation unsuccessful")
+	}
+	return &pb.CreateAuthorResponse{
+		Author: &pb.Author{
+			Authorid:  author.GetAuthorid(),
+			FirstName: author.GetFirstName(),
+			LastName:  author.GetLastName(),
+		},
+	}, nil
+}
+
+// Метод отвечающий за получение записи об авторе из таблицы Author
+func (*server) GetAuthor(ctx context.Context, req *pb.ReadAuthorRequest) (*pb.ReadAuthorResponse, error) {
+	fmt.Println("Read Author", req.GetAuthorid())
+	var author Author
+	res := DB.Find(&author, "author_id = ?", req.GetAuthorid())
+	if res.RowsAffected == 0 {
+		return nil, errors.New("Author not found")
+	}
+	return &pb.ReadAuthorResponse{
+		Author: &pb.Author{
+			Authorid:  author.AuthorID,
+			FirstName: author.FirstName,
+			LastName:  author.LastName,
+		},
+	}, nil
+}
+
+// Метод отвечающий за получение записей об авторах в таблице Author
+func (*server) GetAuthors(ctx context.Context, req *pb.ReadAuthorsRequest) (*pb.ReadAuthorsResponse, error) {
+	fmt.Println("Read Authors")
+	authors := []*pb.Author{}
+
+	res := DB.Find(&authors)
+	if res.RowsAffected == 0 {
+		return nil, errors.New("Authors not found")
+	}
+	return &pb.ReadAuthorsResponse{
+		Authors: authors,
+	}, nil
+}
+
+// Метод отвечающий за обновление записи об авторе в таблице Author
+func (*server) UpdateAuthor(ctx context.Context, req *pb.UpdateAuthorRequest) (*pb.UpdateAuthorResponse, error) {
+	fmt.Println("Update Author")
+	var author Author
+	reqAuthor := req.GetAuthor()
+
+	res := DB.Model(&author).Where("author_id=?", reqAuthor.Authorid).Updates(
+		Author{FirstName: reqAuthor.FirstName, LastName: reqAuthor.LastName})
+
+	if res.RowsAffected == 0 {
+		return nil, errors.New("Authors not found")
+	}
+
+	return &pb.UpdateAuthorResponse{
+		Author: &pb.Author{
+			Authorid:  author.AuthorID,
+			FirstName: author.FirstName,
+			LastName:  author.LastName,
+		},
+	}, nil
+}
+
+// Метод отвечающий за удаление записи об авторе из таблицы Author (удаление происходит по индексу!)
+func (*server) DeleteAuthor(ctx context.Context, req *pb.DeleteAuthorRequest) (*pb.DeleteAuthorResponse, error) {
+	fmt.Println("Delete Author")
+	var author Author
+	res := DB.Where("author_id=?", req.GetAuthorid()).Delete(&author)
+	if res.RowsAffected == 0 {
+		return nil, errors.New("Author not found")
+	}
+
+	return &pb.DeleteAuthorResponse{
+		Success: true,
+	}, nil
+}
+
+func main() {
+	fmt.Println("gRPC server running ...")
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	pb.RegisterBookServiceServer(s, &server{})
+
+	log.Printf("Server listening at %v", lis.Addr())
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve : %v", err)
+	}
+}
