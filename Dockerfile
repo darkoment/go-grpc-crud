14efba000dba8594fd210abd70f74f3c15f05218
Darkoment <Darkoment@yandex.ru>
2023-08-15 15:56:06 +0300

    Change dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 # Create MySQL Image for GO-GRPC-CRUD-API
-FROM mysql
+FROM mysql:8
 MAINTAINER darkoment@yandex.ru
 
-ENV MYSQL_ROOT_PASSWORD TestOnGo 
+ENV MYSQL_ROOT_PASSWORD=TestOnGo 
 
-ADD dump.sql /docker-entrypoint-initdb.d
+COPY ./sql.db/dump.sql /docker-entrypoint-initdb.d
+
+#ADD dump.sql /docker-entrypoint-initdb.d
 
 EXPOSE 3306
--- sql.db/dump.sql
+++ sql.db/dump.sql
@@ -16,6 +16,60 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `author`
+--
+
+CREATE DATABASE test;
+USE test;
+
+DROP TABLE IF EXISTS `author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `author` (
+  `author_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `first_name` longtext NOT NULL,
+  `last_name` longtext NOT NULL,
+  PRIMARY KEY (`author_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `author`
+--
+
+LOCK TABLES `author` WRITE;
+/*!40000 ALTER TABLE `author` DISABLE KEYS */;
+INSERT INTO `author` VALUES (1,'Стивен','Скиена'),(2,'Майкл','Керрикс'),(3,'Авенир','Уёмов'),(4,'Степан','Громов'),(5,'Анатолий','Громов'),(6,'Светлана','Селькова');
+/*!40000 ALTER TABLE `author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `book`
+--
+
+DROP TABLE IF EXISTS `book`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `book` (
+  `book_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` longtext NOT NULL,
+  `year` longtext NOT NULL,
+  `edition` longtext NOT NULL,
+  PRIMARY KEY (`book_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book`
+--
+
+LOCK TABLES `book` WRITE;
+/*!40000 ALTER TABLE `book` DISABLE KEYS */;
+INSERT INTO `book` VALUES (1,'Логические ошибки','2017','1'),(2,'LINUX API исчерпывающее руководство','2017','1'),(3,'Сказки громовых','1999','1'),(4,'Алгоритмы. Руководство по разработке','2022','3'),(5,'Роман Светы','2003','1'),(6,'Наука о данных','2020','2'),(7,'Задачник','1999','3'),(8,'Логика','2003','4'),(9,'Громовержцы','2011','2');
+/*!40000 ALTER TABLE `book` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `book_author`
 --
 
@@ -51,4 +105,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-08-15 12:42:44
+-- Dump completed on 2023-08-15 12:44:08
